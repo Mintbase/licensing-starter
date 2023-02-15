78dbad00865e00861cafd43a78654a637875c746
cif <email@benipsen.com>
2023-02-15 15:59:03 +0000

    feat: :building_construction: Initial starting point w RDF form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WalletContextProvider } from '@mintbase-js/react'
+import { Poppins } from '@next/font/google';
+import '@/styles/globals.css'
+import '@near-wallet-selector/modal-ui/styles.css';
+import 'radix-declarative-form/rdf.css';
+
+const poppins = Poppins({
+  weight: '200',
+  subsets: ['latin']
+});
+
+const poppinsBold = Poppins({
+  weight: '900',
+  subsets: ['latin']
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+          :root {
+            --poppins: ${poppins.style.fontFamily};
+            --poppins-bold: ${poppinsBold.style.fontFamily};
+          }
+        `}</style>
+
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </>
+  )
 }
