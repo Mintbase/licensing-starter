efb06e19279f365197f9cc697964c104ecd0075c
cif <email@benipsen.com>
2023-02-28 09:42:47 -0700

    style: :lipstick: Nicer login prompt

--- components/PromptLogin.tsx
+++ components/PromptLogin.tsx
@@ -1,7 +1,17 @@
+import { useWallet } from "@mintbase-js/react"
 
 
 export const PromptLogin = () => {
+  const { connect } = useWallet();
+
   return (
-    <div className="prompt-login">Please connect a wallet to continue</div>
+    <div className="prompt-login">
+      <button onClick={connect}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3" />
+        </svg>
+      </button>
+      <span>Connect your NEAR account to create</span>
+    </div>
   )
 }
