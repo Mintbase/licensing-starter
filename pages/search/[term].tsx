52ecf2ad78ca2c2fb2b709bc967043874822ce61
cif <email@benipsen.com>
2023-03-01 10:18:29 -0700

    style: :children_crossing: Show loader while loading search results

--- pages/search/[term].tsx
+++ pages/search/[term].tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { NextPageContext } from "next";
 import { useSearch } from "@/hooks/useSearch";
+import { Loader } from "@/components/Loader";
 
 type Props = {
   term: string
@@ -24,18 +25,21 @@ export default function Search({ term }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={"main"}>
-        <div className="search-results">
-          <div className="search-results-center">
-            <h1>Search results for <span>{term}</span></h1>
-          </div>
-        </div>
-        {!loading && results && results.length > 0
-          ? <Gallery images={results} error={error} loading={loading} />
-          : <div className="gallery no-results">No results were found for that search.</div>
-        }
-        <Footer />
-      </main>
+      {loading
+        ? <Loader />
+        : <main className={"main"}>
+            <div className="search-results">
+              <div className="search-results-center">
+                <h1>Search results for <span>{term}</span></h1>
+              </div>
+            </div>
+            {!loading && results && results.length > 0
+              ? <Gallery images={results} error={error} loading={loading} />
+              : <div className="no-search-results">No results were found for that search.</div>
+            }
+            <Footer />
+          </main>
+      }
     </>
   );
 }
